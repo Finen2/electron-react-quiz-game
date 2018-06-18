d0588041ef9ee58dd393167baafda568656fa8bc
Tim Aro <timaro@live.se>
2018-06-18 17:06:22 +0200

    Adding structure for main parts for loged in view

--- live-quiz/src/components/logedIn/InitialPage.js
+++ live-quiz/src/components/logedIn/InitialPage.js
@@ -5,13 +5,28 @@ import { Card, Button, CardTitle, CardText, Col } from 'reactstrap';
 const LogedIn = (props) => {
   return (
     <div style={schoolThid}>
-      <p>Loged in</p>
 
-    <header>
-      <nav>
-        <Link to='/'><Button color="danger">Logout</Button></Link>
-       </nav>
-    </header>
+      <header style={navigation}>
+        <p>Loged in</p>
+        <nav>
+          <Link to='/'><Button color="danger">Logout</Button></Link>
+         </nav>
+      </header>
+
+      <div style={mainContent}>
+        <div style={mainParts}>
+          <p>Play</p>
+        </div>
+        <div style={mainParts}>
+          <p>Learn</p>
+        </div>
+        <div style={mainParts}>
+          <p>Additinal material</p>
+        </div>
+        <div style={mainParts}>
+          <p>More</p>
+        </div>
+      </div>
 
     </div>
   );
@@ -25,5 +40,28 @@ var schoolThid = {
   margin : '0',
   padding : '0',
   height : '100vh',
-  color : 'white'
+  color : 'white',
+  display: 'flex',
+}
+
+var navigation = {
+  width: '8vw',
+  height: '100vh',
+  background: 'pink'
+}
+
+var mainContent = {
+  width: 'calc(100vw - 8vw)',
+  height: '100vh',
+  background: 'yellow',
+  display: 'flex',
+  flexWrap: 'wrap',
+  paddingTop: '7.5vh'
+}
+
+var mainParts = {
+  marginLeft: '7.5%',
+  width: '40%',
+  height: '40vh',
+  background: 'grey'
 }
